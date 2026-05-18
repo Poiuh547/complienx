1488f2d15a8df08ea446d5d8192ed8907975589e
Poiuh547 <61447549+Poiuh547@users.noreply.github.com>
2026-05-18 17:23:11 -0600

    backend: register approval routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authRouter } from "./modules/auth/auth.routes";
 import { usersRouter } from "./modules/users/users.routes";
 import { documentsRouter } from "./modules/documents/documents.routes";
+import { approvalsRouter } from "./modules/approvals/approvals.routes";
 import { actionsRouter } from "./modules/actions/actions.routes";
 import { tasksRouter } from "./modules/tasks/tasks.routes";
 
@@ -10,5 +11,6 @@ export const routes = Router();
 routes.use("/auth", authRouter);
 routes.use("/users", usersRouter);
 routes.use("/documents", documentsRouter);
+routes.use("/approvals", approvalsRouter);
 routes.use("/actions", actionsRouter);
 routes.use("/tasks", tasksRouter);
