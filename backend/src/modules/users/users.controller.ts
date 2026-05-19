98b390b6e529f4e1dc44690567b47e858ddd55b8
Poiuh547 <61447549+Poiuh547@users.noreply.github.com>
2026-05-19 12:16:15 -0600

    backend: add user management controllers

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,6 +1,7 @@
 import type { RequestHandler } from "express";
 import { HttpError } from "../../utils/http-error";
-import { getCurrentUser } from "./users.service";
+import { createUserSchema, updateUserSchema } from "./users.schemas";
+import { createUser, getCurrentUser, listUsers, updateUser } from "./users.service";
 
 export const me: RequestHandler = async (req, res, next) => {
   try {
@@ -10,6 +11,35 @@ export const me: RequestHandler = async (req, res, next) => {
 
     const user = await getCurrentUser(req.user.id);
 
+    res.json({ user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUsers: RequestHandler = async (_req, res, next) => {
+  try {
+    const users = await listUsers();
+    res.json({ users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const postUser: RequestHandler = async (req, res, next) => {
+  try {
+    const input = createUserSchema.parse(req.body);
+    const user = await createUser(input);
+    res.status(201).json({ user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const patchUser: RequestHandler = async (req, res, next) => {
+  try {
+    const input = updateUserSchema.parse(req.body);
+    const user = await updateUser(req.params.id, input);
     res.json({ user });
   } catch (error) {
     next(error);
